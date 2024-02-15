c6ea7bc06bdbaaa5ee557e1c655f354e593b2acc
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-02-15 10:44:20 +0530

    Create boxVolume.h

---
+++ week3/boxVolume.h
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+void boxVolume(float length, float width, float height)
+
+    float volume=length * width * height;
+    cout<<"volume of box is : "<<volume<<endl;
+}
