6585ad2f8d190e0154c7e5d4f2f0ed45ff81608b
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-02-17 11:16:41 +0530

    Update boxArea.h

--- week3/boxArea.h
+++ week3/boxArea.h
@@ -2,6 +2,6 @@
 using namespace std;
 void boxArea(float length, float width, float height)
 {
-    float area= (2*(length * width)+(height*length)*(width*height));
+    float area= (2*(length * width)+(height*length)+(width*height));
     cout<<"area of box is : "<<area<<endl;
 }
