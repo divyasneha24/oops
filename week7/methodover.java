bee61f831b327e98bc672b5d6eb78a4e011a0e5e
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-03-16 11:15:44 +0530

    Update methodover.java

--- week7/methodover.java
+++ week7/methodover.java
@@ -1,1 +1,14 @@
-
+class addition{
+    static int add(int a, int b){
+        return a+b;
+    }
+    static int add(int a,int b,int c){
+        return a+b+c;
+    }
+}
+class methodover{
+public static void main (String[] args){
+    System.out.println(addition.add(2,4));
+    System.out.println(addition.add(5,6,7));
+}
+}
