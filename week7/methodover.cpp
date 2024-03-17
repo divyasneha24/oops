31d7e191f0247fa21a84fbf0f0c8c12ca7aab9e4
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-03-17 16:53:57 +0530

    Create methodover.cpp

---
+++ week7/methodover.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Opoverload{
+    public:
+    int add(int x,int y){
+    return x+y;
+    }
+    float add(float x,float y,float z){
+        return x+y+z;
+    }   
+};
+int main(){
+    Opoverload addition;
+    cout<<addition.add(1,2)<<endl;
+    cout<<addition.add(2,3,4)<<endl;
+    return 0;
+}
