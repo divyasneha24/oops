a999c099823c844a35793accfb7523c2fc2ec27a
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-04-12 18:54:32 +0530

    Create boxunbox.java

---
+++ week8/boxunbox.java
@@ -0,0 +1,12 @@
+public class BoxAndUnbox {
+public static void main(String[] args)
+{
+//Boxing
+Integer a=new Int(30);
+Integer b=new Int(24);
+//UnBoxing
+int c=b;
+System.out.println("Boxed Value : "+a);
+System.out.println("UnBoxed Value : "+c);
+}
+}
