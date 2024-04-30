14f3a7eae24cbbb671bf71f2158f49bfb9f27c6e
divyasneha24 <158817085+divyasneha24@users.noreply.github.com>
2024-04-30 19:12:20 +0530

    Update mapinterface.java

--- week10/mapinterface.java
+++ week10/mapinterface.java
@@ -1,1 +1,14 @@
-
+import java.util.HashMap;
+import java.util.Map;
+public class mapInterface {
+public static void main(String[] args) {
+Map<String,Integer> obj=new HashMap<String,Integer>();
+obj.put("hi", 1);
+obj.put("hello", 2);
+obj.put("mvgr", 3);
+obj.put("oops", 4);
+for(Map.Entry<String,Integer> m:obj.entrySet()){
+System.out.println(m.getKey()+" = "+m.getValue());
+}
+}
+}
